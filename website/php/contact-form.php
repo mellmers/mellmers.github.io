367c9d3fee29c5e9d215db0ff1ef52cf19d0f7ef
Moritz Ellmers <moritz.ellmers@web.de>
2016-07-31 22:37:24 +0200

    - saving all the stuff

--- website/php/contact-form.php
+++ website/php/contact-form.php
@@ -2,86 +2,110 @@
 
 // CHANGE EMAIL ADDRESS ON LINE 54.
 
-
 if(!$_POST) exit;
 
-// Email address verification, do not edit.
-function isEmail($email) {
-	return(preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i",$email));
+if (!defined('PHP_EOL')) define('PHP_EOL', "\r\n");
+
+$formResult = array();
+$captchaResult = false;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	// Construct the Google verification API request link.
+	$params = array();
+	$params['secret'] = '6LcyxRcTAAAAABakVOh3H39RKBX-ZCCG-7wcHk1N'; // Secret key
+	if (!empty($_POST) && isset($_POST['g-recaptcha-response'])) {
+		$params['response'] = urlencode($_POST['g-recaptcha-response']);
+	}
+	$params['remoteip'] = $_SERVER['REMOTE_ADDR'];
+
+	$params_string = http_build_query($params);
+	$requestURL = 'https://www.google.com/recaptcha/api/siteverify?' . $params_string;
+
+	// Get cURL resource
+	$curl = curl_init();
+
+	// Set some options
+	curl_setopt_array($curl, array(
+		CURLOPT_RETURNTRANSFER => 1,
+		CURLOPT_URL => $requestURL,
+	));
+
+	// Send the request
+	$response = curl_exec($curl);
+	// Close request to clear up some resources
+	curl_close($curl);
+
+	$response = @json_decode($response, true);
+
+	if ($response['success'] === true) {
+        $captchaResult = true;
+	}
 }
 
-if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
+$name     	= $_POST['name'];
+$address    = $_POST['address'];
+$zip     	= $_POST['zip'];
+$town     	= $_POST['town'];
+$email    	= $_POST['email'];
+$mobile		= $_POST['mobile'];
+$phone		= $_POST['phone'];
+$subject  	= $_POST['subject'];
+$message 	= $_POST['message'];
 
-$name     = $_POST['name'];
-$email    = $_POST['email'];
-$subject   = $_POST['subject'];
-$comments = $_POST['comments'];
-$verify   = $_POST['verify'];
+$contact = '';
 
-if(trim($name) == '') {
-	echo '<div class="error_message">You must enter your name.</div>';
-	exit();
-} else if(trim($email) == '') {
-	echo '<div class="error_message">Please enter a valid email address.</div>';
-	exit();
-} else if(!isEmail($email)) {
-	echo '<div class="error_message">You have enter an invalid e-mail address, try again.</div>';
-	exit();
+if($email !== '') {
+	$contact = $email;
 }
-
-if(trim($comments) == '') {
-	echo '<div class="error_message">Please enter your message.</div>';
-	exit();
+else if($mobile !== '') {
+	$contact = $mobile;
+}
+else if($phone !== '') {
+	$contact = $phone;
 }
 
 if(get_magic_quotes_gpc()) {
-	$comments = stripslashes($comments);
+	$message = addslashes($message);
 }
-
 
 // Configuration option.
 // Enter the email address that you want to emails to be sent to.
+$receiver = 'moritz.ellmers@web.de';
 
-//$address = "example@example.com";
-$address = "moritz.ellmers@web.de";
-
-
-// Configuration option.
-// i.e. The standard subject will appear as, "You've been contacted by John Doe."
-
-// Example, $e_subject = '$name . ' has contacted you via Your Website.';
-
-$e_subject = 'You\'ve been contacted by ' . $name . '.';
-
+$e_subject = "Anfrage von Webseite: $subject - $name <$contact>";
 
 // Configuration option.
 // You can change this if you feel that you need to.
 // Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
 
-$e_body = "You have been contacted by $name with regards to $subject, their additional message is as follows." . PHP_EOL . PHP_EOL;
-$e_content = "\"$comments\"" . PHP_EOL . PHP_EOL;
-$e_reply = "You can contact $name via email, $email";
+$e_body = 'Sie wurden von ' . $name . ' über das Kontaktformular kontaktiert. Folgende Nachricht wurde empfangen:' . PHP_EOL . PHP_EOL . PHP_EOL;
+$e_content = "\"$message\"" . PHP_EOL . PHP_EOL . PHP_EOL;
+$e_reply = $name . ' hat folgende Kontaktdaten hinterlegt: ' . PHP_EOL . PHP_EOL;
+$e_reply .= $email !== '' ?  " $email " . PHP_EOL : '';
+$e_reply .= $phone !== '' ?  " $phone " . PHP_EOL : '';
+$e_reply .= $mobile !== '' ?  " $mobile " . PHP_EOL : '';
+$e_reply .= $address !== '' ?  " $address " . PHP_EOL : '';
+$e_reply .= $zip !== '' ?  " $zip " . PHP_EOL : '';
+$e_reply .= $town !== '' ?  " $town " . PHP_EOL : '';
+
+//. PHP_EOL . " $phone " . PHP_EOL . " $mobile \n $address \n $zip \n $town \"";
 
 $msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
 
-$headers = "From: $email" . PHP_EOL;
-$headers .= "Reply-To: $email" . PHP_EOL;
-$headers .= "MIME-Version: 1.0" . PHP_EOL;
-$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
-$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
+$headers = "From: $contact" . PHP_EOL;
+$headers .= "Reply-To: $contact" . PHP_EOL;
+$headers .= 'MIME-Version: 1.0' . PHP_EOL;
+$headers .= 'Content-type: text/plain; charset=utf-8' . PHP_EOL;
+$headers .= 'Content-Transfer-Encoding: quoted-printable' . PHP_EOL;
 
-if(mail($address, $e_subject, $msg, $headers)) {
-
-	// Email has sent successfully, echo a success page.
-
-	echo "<fieldset>";
-	echo "<div id='success_page'>";
-	echo "<h4 class='highlight'>Thank you <strong>$name</strong>, your message has been submitted to us.</h4>";
-	echo "</div>";
-	echo "</fieldset>";
-
+if($captchaResult) {
+	if (mail($receiver, $e_subject, $msg, $headers)) {
+		$formResult['success'] = true;
+	} else {
+		$formResult['error'] = "E-Mail konnte nicht gesendet werden. Probiere es gleich noch einmal.";
+	}
 } else {
-
-	echo 'ERROR!';
-
+	$formResult['error'] = "Re-Captcha fehlerhaft - bist du ein Roboter?";
 }
+
+echo json_encode($formResult);
